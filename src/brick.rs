984bc45d9a1524cc4c5060050b5376b7e983a557
Idan Arye <IdanArye@gmail.com>
2025-06-08 18:16:49 +0300

    Add editor ability to duplicate a tile in a proper distance

--- src/brick.rs
+++ src/brick.rs
@@ -1,7 +1,7 @@
 use avian2d::prelude::*;
 use bevy::prelude::*;
-use bevy_yoleck::prelude::*;
 use bevy_yoleck::vpeol_3d::Vpeol3dPosition;
+use bevy_yoleck::{YoleckDirective, prelude::*};
 
 use crate::picking_up::Pickable;
 use crate::topple_detection::Toppleable;
@@ -13,6 +13,7 @@ impl Plugin for BrickPlugin {
         app.add_yoleck_entity_type({
             YoleckEntityType::new("Brick")
                 .with::<Vpeol3dPosition>()
+                .insert_on_init_during_editor(|| Dupable("Brick"))
                 .insert_on_init(|| (IsBrick, Toppleable::Standing))
         });
 
@@ -31,11 +32,15 @@ impl Plugin for BrickPlugin {
         });
 
         app.add_systems(YoleckSchedule::Populate, populate_brick);
+        app.add_yoleck_edit_system(dup_buttons);
     }
 }
 
 #[derive(Component)]
 pub struct IsBrick;
+
+#[derive(Component)]
+pub struct Dupable(&'static str);
 
 fn populate_brick(
     mut populate: YoleckPopulate<Has<Pickable>, With<IsBrick>>,
@@ -55,5 +60,32 @@ fn populate_brick(
         cmd.insert(Friction::new(0.1));
         cmd.insert(Mass(10.0));
         cmd.insert(GravityScale(5.0));
+    });
+}
+
+fn dup_buttons(
+    mut ui: ResMut<YoleckUi>,
+    edit: YoleckEdit<(&YoleckBelongsToLevel, &Vpeol3dPosition, &Dupable)>,
+    mut writer: EventWriter<YoleckDirective>,
+) {
+    let Ok((belongs_to_level, position, dupable)) = edit.single() else {
+        return;
+    };
+
+    let mut make_button = |ui: &mut egui::Ui, label: &str, direction: Dir3| {
+        if ui.button(label).clicked() {
+            writer.write({
+                YoleckDirective::spawn_entity(belongs_to_level.level, dupable.0, true)
+                    .with(Vpeol3dPosition(position.0 + direction * 3.5))
+                    .modify_exclusive_systems(|queue| queue.clear())
+                    .into()
+            });
+        }
+    };
+
+    ui.horizontal(|ui| {
+        make_button(ui, "<-", Dir3::NEG_X);
+        ui.label("duplicate");
+        make_button(ui, "->", Dir3::X);
     });
 }
